e8dce6daba6011ae244d1cd7a4ff41a6b72c0ae3
Li Bo <libo@espressif.com>
2026-06-12 20:17:44 +0800

    docs(ci): implement push to GitHub CI

---
+++ tools/ci/push_to_github.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+# SPDX-License-Identifier: Apache-2.0
+
+# gitlab-ci script to push current tested revision (tag or branch) to github
+
+set -ex
+
+if [ -n "${CI_COMMIT_TAG}" ]; then
+    # for tags
+    git push github "${CI_COMMIT_TAG}"
+else
+    # for branches
+    git push github "${CI_COMMIT_SHA}:refs/heads/${CI_COMMIT_REF_NAME}"
+fi
