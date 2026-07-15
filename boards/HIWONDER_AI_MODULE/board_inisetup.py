536d7bdccc6d71e4514cf55ef67048701eaed1d4
rainrainrainbow <1448282415@qq.com>
2026-07-15 08:34:54 +0800

    Create HIWONDER_AI_MODULE board_inisetup.py

---
+++ boards/HIWONDER_AI_MODULE/board_inisetup.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2026 Espressif Systems (Shanghai) CO LTD
+#
+# SPDX-License-Identifier: Apache-2.0
+
+MAIN_PY = """\
+import display
+import sensor
+import time
+
+print("ESP-VISION HIWONDER_AI_MODULE ready")
+
+lcd = display.Display()
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time=1000)
+
+while True:
+    img = sensor.snapshot()
+    lcd.write(img)
+    time.sleep_ms(20)
+"""
+
+README_TXT = """\
+ESP-VISION HIWONDER_AI_MODULE
+
+Edit main.py to run your Python vision script.
+Use the ESP-VISION VSCode extension to run scripts and preview frames.
+The default main.py previews the camera image on the board LCD.
+"""
