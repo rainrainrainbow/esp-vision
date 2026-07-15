258e12ee2cc1097560bda430c6784e67a847fd47
rainrainrainbow <1448282415@qq.com>
2026-07-15 10:54:20 +0800

    Fix manifest.py: use PORT_DIR, add board_inisetup

--- boards/HIWONDER_S3/manifest.py
+++ boards/HIWONDER_S3/manifest.py
@@ -1,2 +1,6 @@
 # MicroPython frozen manifest for Hiwonder S3
-freeze("$(TOP)/modules")
+freeze("$(PORT_DIR)/modules")
+freeze("$(ESP_VISION_ROOT)/modules", "py_inisetup.py")
+freeze("$(ESP_VISION_ROOT)/boards/HIWONDER_S3", "board_inisetup.py")
+include("$(MPY_DIR)/extmod/asyncio")
+require("webrepl")
